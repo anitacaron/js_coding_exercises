1ff31d22a73d4f78e2330e387a93db4ee6108b7f
anitacaron <annycaron@gmail.com>
2020-11-28 17:36:41 +0000

    Implemented exercise - countLinuxUsers

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -44,7 +44,12 @@ function reverseAllWords(words) {
 
 function countLinuxUsers(users) {
   if (users === undefined) throw new Error("users is required");
-  // Add your code here!
+  count = 0
+  for (var i = 0; i < users.length; i++) {
+    if (users[i].type == "Linux") count += 1
+  }
+
+  return count
 }
 
 function getMeanScore(scores) {
