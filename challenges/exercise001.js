1d80218f76316573abde327087fa998d1526c49c
anitacaron <annycaron@gmail.com>
2020-11-28 17:26:36 +0000

    Implemented exercise - reverseAllWords

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -1,42 +1,45 @@
 function capitalize(word) {
   if (word === undefined) throw new Error("word is required");
   //return word.charAt(0).toUpperCase() + word.slice(1);
-  return word.substring(0,1).toUpperCase() + word.substring(1, word.length)
+  return word.substring(0,1).toUpperCase() + word.substring(1, word.length);
 }
 
 function generateInitials(firstName, lastName) {
   if (firstName === undefined) throw new Error("firstName is required");
   if (lastName === undefined) throw new Error("lastName is required");
-  return firstName.substring(0,1) + '.' + lastName.substring(0,1)
+  return firstName.substring(0,1) + '.' + lastName.substring(0,1);
 }
 
 function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is requied");
   if (vatRate === undefined) throw new Error("vatRate is required");
-  return Math.round(originalPrice * (1 + (vatRate / 100)) * 100) / 100
+  return Math.round(originalPrice * (1 + (vatRate / 100)) * 100) / 100;
 }
 
 function getSalePrice(originalPrice, reduction) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
   if (reduction === undefined) throw new Error("reduction is required");
-  return originalPrice - Math.round((originalPrice * reduction/100) * 100) / 100
+  return originalPrice - Math.round((originalPrice * reduction/100) * 100) / 100;
 }
 
 function getMiddleCharacter(str) {
   if (str === undefined) throw new Error("str is required");
-  console.log(str.length)
-  if (str.length % 2 == 0) return str.substring(Math.floor(str.length/2)-1,Math.floor(str.length/2) + 1)
-  else return str.charAt(Math.floor(str.length/2))
+  if (str.length % 2 == 0) return str.substring(Math.floor(str.length/2)-1,Math.floor(str.length/2) + 1);
+  else return str.charAt(Math.floor(str.length/2));
 }
 
 function reverseWord(word) {
   if (word === undefined) throw new Error("word is required");
-  return word.split("").reverse().join("")
+  return word.split("").reverse().join("");
 }
 
 function reverseAllWords(words) {
   if (words === undefined) throw new Error("words is required");
-  // Add your code here!
+  for (var i = 0; i < words.length; i++) {
+    let word = words[i];
+    words[i] = word.split("").reverse().join("");
+  }
+  return words
 }
 
 function countLinuxUsers(users) {
